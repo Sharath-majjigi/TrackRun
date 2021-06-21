ae52d8cf6a603becef7499d7817c8534bfbce77f
Sharath Majjigi <sharathholmes143@gmail.com>
2021-06-22 00:02:15 +0530

    DaggerHilt Setup

--- app/src/main/java/com/example/trackrun/MainActivity.kt
+++ app/src/main/java/com/example/trackrun/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.trackrun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import com.example.trackrun.db.RunDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var runDao: RunDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       
     }
 }
